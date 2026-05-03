8d8c5d1a19261c9298753839aface557c9d08703
mraees93 <raeesdolley76@gmail.com>
2026-05-03 17:40:19 +0200

    installed react app, renamed folder

--- url_shortener/obj/Debug/net10.0/url_shortener.AssemblyInfo.cs
+++ url_shortener/obj/Debug/net10.0/url_shortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("url_shortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff86ed50be7b0bc2b063b9b61aac711783d824e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76eea50581fac3f860a46b5a420423b675bdc4bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("url_shortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("url_shortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
