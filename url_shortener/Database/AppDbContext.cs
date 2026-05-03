8da9b376e06ca51e9457e2c43edb0e40cf8e05c1
mraees93 <raeesdolley76@gmail.com>
2026-05-03 11:45:31 +0200

    added dbContext

---
+++ url_shortener/Database/AppDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using url_shortener.Models;
+
+namespace url_shortener.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ensuring that ShortCode is unique for fast lookups
+            modelBuilder.Entity<ShortUrl>()
+                .HasIndex(u => u.ShortCode)
+                .IsUnique();
+        }
+    }
+}
