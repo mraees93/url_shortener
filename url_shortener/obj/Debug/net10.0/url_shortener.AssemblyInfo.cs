67f3b1e51e0e34ea6248ed8c4fc0dbd5ccb4f743
mraees93 <raeesdolley76@gmail.com>
2026-05-06 19:54:42 +0200

    Final: Syncing database models for production launch

--- url_shortener/obj/Debug/net10.0/url_shortener.AssemblyInfo.cs
+++ url_shortener/obj/Debug/net10.0/url_shortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("url_shortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292aafc0d474d534ff7dc54263b677d3791f0792")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7fc83cedd9eeaa06af377fa5d3162abf3bc1d5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("url_shortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("url_shortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
